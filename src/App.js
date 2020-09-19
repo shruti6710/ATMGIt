6959d292c02ddbb08191a304e93490ad92f2c86d
shruti6710 <shruti.r6710@gmail.com>
2020-09-20 00:23:45 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import logo from './ga.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Account from './Account';
+
+class App extends Component {
+  render() {
+    return (
+      <div id="content">
+        <div id="nav">
+          <div id="logo"><img src={logo} alt="General Assembly logo" /></div>
+          <div id="title">Pieces Of I Owe You</div>
+        </div>
+        <Account name="Checking" />
+        <Account name="Savings"/>
+        <div className="clear"></div>
+      </div>
+    );
+  }
 }
 
 export default App;
